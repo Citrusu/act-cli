7ffee99f508736217321636bab51fcf84d22abcf
Citrus <zhouyou421@126.com>
2017-07-18 09:47:25 +0800

    source

--- index.js
+++ index.js
@@ -5,41 +5,104 @@ const fs = require('fs');
 
 const config = require('./config.json');
 
-fs.readFile('./template/js/demo.js', 'utf-8', function(err, data){
-    // console.log(data.replace(/{{name}}/g, config.name));
-    let page = data.replace(/{{Date}}/g, new Date());
-    fs.writeFile('./dist/page.js', page, function(err){
-        if(err){
-            return console.log(err);
-        }
-    });
-});
+function init(){
+    let list = config.fileList;
+    let map = config.page;
+    list.forEach(function(n, i){
+        let dist = map.dist.html;
+        checkFile(dist, function(){
+            // let temp = readFile(map.src.html);
+            // console.log(temp);return false;
+            // let page = useFile(temp);
+            // saveFile(map.dist.html + config.module, page);
+        });
+    })
+}
+init();
 
 /*
 * 检查文件状态
 * 为了安全，已经存在的文件不会生成
 * */
-function fileStatus() {
-    
+function checkFile(src, func) {
+    //创建多层目录
+    let srcArr = src.split('/');
+    srcArr.splice(0,1);
+    if(srcArr[srcArr.length - 1] === ''){
+        srcArr.splice(srcArr.length - 1 ,1);
+    }
+    // console.log(srcArr);
+    let srcStr = './';
+    srcArr.forEach((n, i) => {
+        srcStr += n + '/';
+        //检查目录是否存在，不存在则创建
+        if(!fs.existsSync(srcStr)){
+            fs.mkdir(srcStr, () => {
+                console.log(`创建目录 ${srcStr}`);
+            });
+        }
+    });
+    return false;
+    fs.stat(src, function(err, stats){
+        // console.log(src);
+
+        if(err){
+            fs.mkdirSync('./test/src/', function(err){
+                if(err){
+                    console.log(err);
+                    return false;
+                }
+                console.log('创建目录' + src);
+            });
+        };
+
+        // console.log(stats)
+        // if(stats.isFile()){
+        //     console.log(`${src} 已经存在`);
+        // }else{
+            //func();
+        // }
+    })
 }
 
 /*
 * 读取文件
 * */
-function readFile(file){
-
+function readFile(src){
+    fs.readFile(src, 'utf-8', function(err, data){
+        if(err){
+            console.log(err)
+        }else{
+            return data;
+        }
+    });
 }
 
 /*
 * 操作文件
 * */
 function useFile(file){
+    let rule = [
+        ['Date', new Date()],
+        ['title', config.name]
+    ];
+    let page = file;
+    rule.forEach(function(n, i){
+        page.replace(/{{"+ n[0] +"}}/g, n[1]);
+    });
 
+    return page;
 }
 
 /*
 * 保存文件
 * */
-function saveFile(file){
-
+function saveFile(dist, fileData){
+    fs.writeFile(dist, fileData, function(err){
+        if(err){
+            return console.log(err);
+        }else{
+            console.log(`已创建 ${dist}`);
+        }
+    });
 }
