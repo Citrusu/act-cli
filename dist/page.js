0910b15d7ea9de7e3d9237a42d4d698a5e3b25ef
Citrus <zhouyou421@126.com>
2017-07-19 10:17:39 +0800

    add  generate other files

--- dist/page.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Act_cli on Mon Jul 17 2017 14:59:06 GMT+0800 (CST).
- */
-define(function(require, exports, module) {
-
-
-});
--- index.js
+++ index.js
@@ -2,26 +2,46 @@
  * Created by Citrus on 17/07/2017.
  */
 const fs = require('fs');
+const path = require('path');
 
 const config = require('./config.json');
 
-function init(){
-    let module = config.module;
+let act = config.act;
+
+/*
+* 默认
+* 根据 page 创建 html,js,css，生成默认模板
+* */
+function getBase() {
     let fileList = config.fileList;
     let map = config.page;
     let dists = map.dist;
     for(let key in dists){
         fileList.forEach(async (n, i) => {
-            let dist = dists[key] + module +'/'+ n.fileName + '.' + key;
+            let dist = dists[key] + act +'/'+ n.fileName + '.' + key;
             mkDir(dist);
             let tem = await readFile(map.src[key]);
             let page = useFile(tem, n.fileName);
             saveFile(dist, page);
         })
     }
-
 }
-init();
+
+/*
+* 生成其它文件，copy 模板到目录
+* */
+async function copyFile(){
+    let fileList = config.outs;
+    fileList.forEach(async (n, i) => {
+        let src = n.src;
+        let file = path.parse(src).base;
+        let dist = config.base + n.dist + act +'/' + file;
+
+        let tem = await readFile(src);
+        saveFile(dist, tem);
+    })
+}
+
 
 /*
 * 检查/创建目录
@@ -29,15 +49,15 @@ init();
 function mkDir(src) {
     //创建多层目录
     let srcArr = src.split('/');
-    srcArr.splice(0,1);
+    // srcArr.splice(0,1);
     srcArr.splice(srcArr.length - 1, 1);
-    let srcStr = './';
-    srcArr.forEach(async (n, i) => {
+    let srcStr = config.base;
+    srcArr.forEach((n, i) => {
         srcStr += n + '/';
         // console.log('检查目录'+srcStr);
         //检查目录是否存在，不存在则创建
         if(!fs.existsSync(srcStr)){
-            await fs.mkdir(srcStr, () => {
+            fs.mkdirSync(srcStr, () => {
                 console.log(`创建目录 ${srcStr}`);
             });
 
@@ -50,7 +70,7 @@ function mkDir(src) {
 * */
 function readFile(src){
     return new Promise((resolved, reject) => {
-        fs.readFile(src, 'utf-8', function(err, data){
+        fs.readFile(src, 'utf-8', (err, data) => {
             if(err){
                 console.log(err);
             }else{
@@ -66,7 +86,7 @@ function readFile(src){
 function useFile(file, fileName){
     let rules = {
         Date: new Date(),
-        module: config.module,
+        act: config.act,
         page: fileName,
         wxshare: '<include file="_wxshare" />'
     };
@@ -88,7 +108,7 @@ function saveFile(dist, fileData){
         console.log(`已存在 ${dist}`);
         return false;
     }
-    fs.writeFile(dist, fileData, function(err){
+    fs.writeFile(dist, fileData, (err) => {
         if(err){
             return console.log(err);
         }
@@ -96,3 +116,9 @@ function saveFile(dist, fileData){
         console.log(`已创建 ${dist}`);
     });
 }
+
+function init(){
+    getBase();
+    copyFile();
+}
+init();
